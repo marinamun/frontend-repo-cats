1345d8bed74b53cf3082d4e814cf9e640338859e
Marina Muñoz <marinacerejidoo@gmail.com>
2023-10-12 11:21:12 +0200

    Improved About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,15 +4,16 @@ import "../App.css";
 function About() {
   return (
     <div className="about">
+      <br/>
       <h1>The founder:</h1>
       <img
         src="https://comicvine.gamespot.com/a/uploads/scale_small/11/111746/6556040-crazy_cat_lady.png"
-        style={{ width: "80px" }}
+        style={{ width: "100px" }}
         className="cruella-photo"
       />
 
       <h5>
-        Hi there, I'm the mastermind of CatMarket. I prefer to keep my name private because some people say that what I do is not entirely legal. As you can see from my photo, I'm a lovely lady that has always dedicated her life to her cats. 
+        Hi there, I'm the mastermind of <strong>CatMarket</strong>. I prefer to keep my name private because some people say that what I do is not entirely legal... As you can see from my photo, I'm a lovely lady that has always dedicated her life to her cats. 
       </h5>
       <p>
         PS: Don't bother because you won't find any contact details.
