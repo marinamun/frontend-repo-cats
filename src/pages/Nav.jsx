9d1343587c622289f1fa974e8cf0868566005e0d
John Doe <johndoe@example.com>
2023-10-12 13:21:53 +0200

    I fixed the Navbar

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,78 +1,80 @@
-import { Link } from "react-router-dom";
-import "../pages/Nav.css"
+import { NavLink } from "react-router-dom";
+import "../pages/Nav.css";
+
 export const Nav = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-white rounded nav">
-      <div className="container-fluid">
-        <Link to={`/`}>
-          {" "}
+    <div className="navbar-container">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <nav className="navbar navbar-expand-lg bg-white rounded nav">
+        <NavLink to={`/`} activeClassName="active">
           <img
             src="https://logowik.com/content/uploads/images/cute-cat-clipart712.logowik.com.webp"
             style={{ width: "60px" }}
           />
-        </Link>
-
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link
-                to="/cats"
-                className="nav-link"
-                style={{ marginLeft: "10px", marginRight: "10px" }}
-              >
-                Buy a cat
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/Sellyourcat"
-                className="nav-link"
-                style={{ marginLeft: "10px", marginRight: "10px" }}
-              >
-                Sell your cat
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/about"
-                className="nav-link"
-                style={{ marginLeft: "10px", marginRight: "10px" }}
-              >
-                Our story
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/Login"
-                className="nav-link"
-                style={{ marginLeft: "10px", marginRight: "10px" }}
-              >
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/shopping-cart"
-                className="nav-link"
-                style={{ marginLeft: "10px", marginRight: "10px" }}
-              >
-                Shopping cart
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+        </NavLink>
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <NavLink
+              to="/cats"
+              className="nav-item"
+              activeClassName="active"
+              style={{ marginLeft: "10px", marginRight: "10px" }}
+            >
+              Buy a cat
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/Sellyourcat"
+              className="nav-item"
+              activeClassName="active"
+              style={{ marginLeft: "10px", marginRight: "10px" }}
+            >
+              Sell your cat
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/about"
+              className="nav-item"
+              activeClassName="active"
+              style={{ marginLeft: "10px", marginRight: "10px" }}
+            >
+              Our story
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/Login"
+              className="nav-item"
+              activeClassName="active"
+              style={{ marginLeft: "10px", marginRight: "10px" }}
+            >
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/shopping-cart"
+              className="nav-item"
+              activeClassName="active"
+              style={{ marginLeft: "10px", marginRight: "10px" }}
+            >
+              Shopping cart
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
